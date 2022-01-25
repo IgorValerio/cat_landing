7b8ab1472d5e06803e18ec42cbb373c58ebef6a6
Igor <igorv386@gmail.com>
2022-01-25 16:28:56 -0300

    Criando a HomeController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,13 +1,11 @@
 <?php 
 
+namespace App\Controllers;
 
-
-
-
-
-
-
-
-
-
-?>
+class HomeController
+{
+  public function index($request, $response)
+  {
+      return $response->write('Hello, World!' . $request->getParam('name'));
+  }
+}
--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,2 @@
 <?php 
-$app->get('/', function($request, $response) {
-  return 'Hello, World!';
-}); 
+$app->get('/', 'HomeController:index');
