f6523b97d5d6de534a3f0a8c46aea8141fab254b
Igor <igorv386@gmail.com>
2022-01-25 18:20:26 -0300

    Configurando o Twig Template

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,6 +7,6 @@ class HomeController extends Controller
 
   public function index($request, $response)
   {
-      return $response->write($this->container->hello);
+      return $this->container->view->render($response, 'index.twig');
   }
 }
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,9 +6,26 @@ date_default_timezone_set('America/Sao_Paulo');
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new Slim\App();
+$app = new Slim\App([
+  'settings' => [
+      'displayErrorDetails' => true
+  ]
+]);
 
 $container = $app->getContainer();
+
+$container['view'] = function($container) {
+    $view = new Slim\Views\Twig(__DIR__ . '/../resources/views', [
+      'cache' => false,
+    ]);
+
+    $view->addExtension(new Slim\Views\TwigExtension(
+      $container->router,
+      $container->request->getUri()
+    ));
+
+    return $view;
+};    
 
 $container['hello'] = "Hello, World.";
 
