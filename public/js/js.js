01e4bdbba084e8ff8a31b1ab2d86313f00de2de4
Igor <igorv386@gmail.com>
2022-02-16 18:26:36 -0300

    Criando efeito no menu com Jaquery

---
+++ public/js/js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.btn-menu').on('click', function(){
+      toggleMenu($('.menus'));
+    });
+    $('.menus').before().on('click', function(){
+      toggleMenu($('.menus'));
+    });
+
+    function toggleMenu(menu) {
+      menu.toggleClass('active');
+    }
+});
